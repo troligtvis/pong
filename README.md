dbc1280fdddda10f02d75e172ee52ae8fed01e2c
Troligtvis <karljohan@codefork.se>
2020-08-16 21:16:59 +0200

    Change engine from ggez to bevy

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # Pong-A-Long
-Atari's 1972 classic pong, made with rust.
-
-![demo](pong-a-long.gif)
+Atari's 1972 classic pong, made with rust. 
 
 # Background
-The project is first and foremost about learning `Rust`. But also about getting my hands dirty with game development. Starting slow without much help. The engine used in this project is [ggez](https://ggez.rs/).
-Future games I will explore and recreate will be _Snake_ and _Tetris_. 
+I found a new engine written in Rust, [bevy](https://bevyengine.org/), that I wanted to try out. The development on the engine is in its early stages and with it there is a lot of things that might change with the API and bugs that is found during playing around with it.
 
 # Setup
 Install [rust](https://www.rust-lang.org/learn/get-started) + cargo
@@ -15,14 +12,5 @@ Clone the repo to your desktop
 # Usage 
 Go to the `root` folder and execute `cargo run` 
 
-# Future features and development
-- Options menu to set max score, paddle size, speed
-- Items in the game which will dynamically change the game state
-- About page, describing a bit about the project
-- Be able to read from file
-- Transition animation between scenes
-- Explore [WASM](https://www.rust-lang.org/what/wasm)
-- Upload to [itch.io](https://itch.io/) (using wasm)
-
 # Known bugs 
-- Collisions for the paddles are a bit off sometimes
+- 
--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,269 @@
-use std::{env, path};
+use bevy::{
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
+use rand::{self, thread_rng, Rng};
 
-use ggez;
-use ggez::event;
-use ggez::graphics;
-use ggez::graphics::{DrawParam, Text};
-use ggez::input::keyboard::{self, KeyCode, KeyMods};
-use ggez::nalgebra as na;
-use ggez::{Context, GameResult};
+// due to not being able to access windows from a `startup_system`
+// fixed values will be needed for screen size during startup
+// https://github.com/bevyengine/bevy/issues/175
+const SCR_WIDTH: f32 = 800.0; 
+const SCR_HEIGHT: f32 = 600.0;
 
-mod ball;
-mod collidable;
-mod player;
-mod scenes;
-mod util;
-mod world;
+const PADDING: f32 = 40.0;
+const PADDLE_HEIGHT: f32 = 120.0;
+const PADDLE_WIDTH: f32 = 12.0;
 
-use ball::Ball;
-use player::{Controls, Paddle};
-use scenes::*;
-use world::World;
-
-struct MainState {
-    world: World,
-    current_scene: Box<dyn Scene<World>>,
+fn randomize_vec(vec: &mut Vec3, x: f32, y: f32) {
+    let mut rng = thread_rng();
+    *vec.x_mut() = if rng.gen_bool(0.5) { x } else { -x };
+    *vec.y_mut() = if rng.gen_bool(0.5) { y } else { -y };
 }
 
-impl MainState {
-    pub fn new(ctx: &mut Context) -> Self {
-        let initial_scene = Box::new(scenes::menu::MenuScene::new(ctx));
-
-        let max_score = 5;
-        let world = World::new(ctx, max_score);
-
-        MainState {
-            world,
-            current_scene: initial_scene,
-        }
-    }
+struct KeyboardControls {
+    up: KeyCode,
+    down: KeyCode,
 }
 
-impl event::EventHandler for MainState {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-        let dt = ggez::timer::delta(ctx).as_secs_f32();
-        self.world.update_delta_time(dt);
-
-        if let Some(next_scene) = self.current_scene.update(ctx, &mut self.world) {
-            self.current_scene = next_scene
-        }
-
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
-
-        self.current_scene.draw(ctx, &mut self.world);
-
-        graphics::present(ctx)?;
-        Ok(())
-    }
-
-    fn key_down_event(
-        &mut self,
-        ctx: &mut Context,
-        keycode: KeyCode,
-        _keymod: KeyMods,
-        repeat: bool,
-    ) {
-        if keycode == KeyCode::Escape {
-            event::quit(ctx)
-        }
-
-        self.current_scene
-            .input(ctx, &mut self.world, keycode, true, repeat)
-    }
+enum Collider {
+    Solid, 
+    Scorable,
 }
 
-fn main() -> GameResult {
-    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
-        let mut path = path::PathBuf::from(manifest_dir);
-        path.push("resources");
-        path
+struct Ball {
+    velocity: Vec3,
+}
+
+struct Paddle {
+    speed: f32,
+    controls: KeyboardControls, 
+}
+
+fn setup(
+    mut commands: Commands, 
+    mut materials: ResMut<Assets<ColorMaterial>>, 
+    _asset_server: Res<AssetServer>
+) {
+    commands
+        .spawn(Camera2dComponents::default())
+        .spawn(UiCameraComponents::default());
+
+    // Player 1
+    commands
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
+            translation: Translation(Vec3::new(-SCR_WIDTH / 2.0 + PADDING, 0.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT),
+            },
+            ..Default::default()
+        })
+        .with(Paddle {
+            speed: 500.0,
+            controls: KeyboardControls { up: KeyCode::W, down: KeyCode::S }
+        })
+        .with(Collider::Solid);
+
+    // Player 2
+    commands
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(0.4, 0.4, 0.4).into()),
+            translation: Translation(Vec3::new(SCR_WIDTH / 2.0 - PADDING, 0.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT),
+            },
+            ..Default::default()
+        })
+        .with(Paddle {
+            speed: 500.0,
+            controls: KeyboardControls { up: KeyCode::Up, down: KeyCode::Down }
+        })
+        .with(Collider::Solid);
+
+    // Ball 
+
+    let mut velocity = Vec3::zero();
+    randomize_vec(&mut velocity, 200.0, 200.0);
+
+    commands
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+            translation: Translation(Vec3::zero()),
+            sprite: Sprite {
+                size: Vec2::new(20.0, 20.0),
+            },
+            ..Default::default()
+        })
+        .with(Ball {
+            velocity,
+        })
+        .with(Collider::Solid);
+
+    // Walls
+    let wall_material = materials.add(Color::rgb(1.0, 1.0, 1.0).into());
+    let wall_thickness = 10.0;
+    let bounds = Vec2::new(SCR_WIDTH, SCR_HEIGHT);
+
+    commands
+        // left
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(-bounds.x() / 2.0, 0.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(wall_thickness, bounds.y() + wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Collider::Scorable)
+        // right
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(bounds.x() / 2.0, 0.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(wall_thickness, bounds.y() + wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Collider::Scorable)
+        // bottom
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(0.0, -bounds.y() / 2.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(bounds.x() + wall_thickness, wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Collider::Solid)
+        // top
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(0.0, bounds.y() / 2.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(bounds.x() + wall_thickness, wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Collider::Solid);
+}
+
+fn paddle_movement_system(
+    time: Res<Time>,
+    windows: Res<Windows>,
+    keyboard_input: Res<Input<KeyCode>>, 
+    mut query: Query<(&Paddle, &mut Translation)>
+) {
+
+    let scr_height: f32 = if let Some(window) = windows.get_primary() {
+        window.height as f32
     } else {
-        path::PathBuf::from("./resources")
+        SCR_HEIGHT
     };
 
-    let c = ggez::conf::Conf::new();
-    let (ref mut ctx, ref mut event_loop) = ggez::ContextBuilder::new("Pong", "Troligtvis")
-        .add_resource_path(resource_dir)
-        .conf(c)
-        .build()
-        .unwrap();
+    for (paddle, mut translation) in &mut query.iter() {
+        let mut direction = 0.0;
+        if keyboard_input.pressed(paddle.controls.up) {
+            direction += 1.0;
+        }
 
-    graphics::set_window_title(ctx, "PONG");
+        if keyboard_input.pressed(paddle.controls.down) {
+            direction -= 1.0;
+        }
 
-    let mut state = MainState::new(ctx);
-    event::run(ctx, event_loop, &mut state)
+        *translation.0.y_mut() += time.delta_seconds * direction * paddle.speed;
+
+        // bound the paddle to screen
+        *translation.0.y_mut() = f32::max(
+            -scr_height / 2.0 + PADDLE_HEIGHT / 2.0, 
+            f32::min(scr_height / 2.0 - PADDLE_HEIGHT / 2.01, translation.0.y())
+        );
+    }
+}
+
+fn ball_movement_system(
+    time: Res<Time>,
+    _windows: Res<Windows>,
+    mut query: Query<(&Ball, &mut Translation)>
+) {
+    for (ball, mut translation) in &mut query.iter() {
+        translation.0 += ball.velocity * time.delta_seconds; 
+    }
+}
+
+fn ball_collision_system(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>, 
+    mut ball_query: Query<(Entity, &mut Ball, &Translation, &Sprite)>,
+    mut collider_query: Query<(Entity, &Collider, &Translation, &Sprite)>,
+) {
+    for (ball_entity, mut ball, ball_translation, sprite) in &mut ball_query.iter() {
+        let ball_size = sprite.size;
+        let velocity = &mut ball.velocity;
+
+        // check collision with walls
+        for (_collider_entity, collider, translation, sprite) in &mut collider_query.iter() {
+            let collision = collide(ball_translation.0, ball_size, translation.0, sprite.size);
+            if let Some(collision) = collision {
+                if let &Collider::Scorable = collider {
+                    commands.despawn(ball_entity);
+                    
+                    let mut velocity = Vec3::zero();
+                    randomize_vec(&mut velocity, 200.0, 200.0);
+                    commands
+                        .spawn(SpriteComponents {
+                            material: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+                            translation: Translation(Vec3::zero()),
+                            sprite: Sprite {
+                                size: Vec2::new(20.0, 20.0),
+                            },
+                            ..Default::default()
+                        })
+                        .with(Ball { 
+                            velocity
+                        })
+                        .with(Collider::Solid);
+                }
+
+                // reflect the ball when it collides
+                let mut reflect_x = false;
+                let mut reflect_y = false;
+
+                // only reflect if the ball's velocity is going in the opposite direction of the collision
+                match collision {
+                    Collision::Left => reflect_x = velocity.x() > 0.0,
+                    Collision::Right => reflect_x = velocity.x() < 0.0,
+                    Collision::Top => reflect_y = velocity.y() < 0.0,
+                    Collision::Bottom => reflect_y = velocity.y() > 0.0,
+                }
+
+                // reflect velocity on the x-axis if we hit something on the x-axis
+                if reflect_x {
+                    *velocity.x_mut() = -velocity.x();
+                }
+
+                // reflect velocity on the y-axis if we hit something on the y-axis
+                if reflect_y {
+                    *velocity.y_mut() = -velocity.y();
+                }
+
+                break;
+            }
+        }
+    }
+}
+
+fn main() {
+    App::build()
+        .add_resource(WindowDescriptor {
+            title: "Bevy Pong".to_string(),
+            width: SCR_WIDTH as u32,
+            height: SCR_HEIGHT as u32,
+            vsync: true,
+        })
+        .add_default_plugins()
+        .add_startup_system(setup.system()) 
+        .add_system(paddle_movement_system.system())
+        .add_system(ball_movement_system.system())
+        .add_system(ball_collision_system.system())
+        .run();
 }
